0eef67ed3e452228565b287d32dd82c79dd9768f
Hamza Koc <hamzakooc@gmail.com>
2023-07-05 15:10:15 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>Hello World{value}</div>;
+  return <div>Hello World how are you{value}</div>;
 }
 
 export default App;
